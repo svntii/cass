5bc76e8ad6b9344b6dde757b56cfeb9fc2cac569
Santiago Rodriguez <svntiiago@gmail.com>
2024-04-13 23:23:47 -0400

    forgive me father for I have sinned

--- react/db/preference.ts
+++
@@ -1,8 +0,0 @@
-import { Culture, Language, Religion } from "@/types/content-types";
-
-export interface Preference {
-    religion: Religion[];
-    culture: Culture[];
-    languages: Language[];
-
-}
